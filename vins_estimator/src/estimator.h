63b1e734371c2a6d36e2e95482a0fae942db32c8
Elia Kaufmann <elia.kaufmann92@gmail.com>
2019-10-28 19:16:16 +0100

    wip

--- vins_estimator/src/estimator.h
+++ vins_estimator/src/estimator.h
@@ -23,117 +23,129 @@
 #include <opencv2/core/eigen.hpp>
 
 
-class Estimator
-{
-  public:
-    Estimator();
+class Estimator {
+public:
+  Estimator();
 
-    void setParameter();
+  void setParameter();
 
-    // interface
-    void processIMU(double t, const Vector3d &linear_acceleration, const Vector3d &angular_velocity);
-    void processImage(const map<int, vector<pair<int, Eigen::Matrix<double, 7, 1>>>> &image, const std_msgs::Header &header);
-    void setReloFrame(double _frame_stamp, int _frame_index, vector<Vector3d> &_match_points, Vector3d _relo_t, Matrix3d _relo_r);
+  // interface
+  void processIMU(double t, const Vector3d& linear_acceleration, const Vector3d& angular_velocity);
 
-    // internal
-    void clearState();
-    bool initialStructure();
-    bool visualInitialAlign();
-    bool relativePose(Matrix3d &relative_R, Vector3d &relative_T, int &l);
-    void slideWindow();
-    void solveOdometry();
-    void slideWindowNew();
-    void slideWindowOld();
-    void optimization();
-    void vector2double();
-    void double2vector();
-    bool failureDetection();
+  void
+  processImage(const map<int, vector<pair<int, Eigen::Matrix<double, 7, 1>>>>& image, const std_msgs::Header& header);
 
+  void setReloFrame(double _frame_stamp, int _frame_index, vector<Vector3d>& _match_points, Vector3d _relo_t,
+                    Matrix3d _relo_r);
 
-    enum SolverFlag
-    {
-        INITIAL,
-        NON_LINEAR
-    };
+  // internal
+  void clearState();
 
-    enum MarginalizationFlag
-    {
-        MARGIN_OLD = 0,
-        MARGIN_SECOND_NEW = 1
-    };
+  bool initialStructure();
 
-    SolverFlag solver_flag;
-    MarginalizationFlag  marginalization_flag;
-    Vector3d g;
-    MatrixXd Ap[2], backup_A;
-    VectorXd bp[2], backup_b;
+  bool visualInitialAlign();
 
-    Matrix3d ric[NUM_OF_CAM];
-    Vector3d tic[NUM_OF_CAM];
+  bool relativePose(Matrix3d& relative_R, Vector3d& relative_T, int& l);
 
-    Vector3d Ps[(WINDOW_SIZE + 1)];
-    Vector3d Vs[(WINDOW_SIZE + 1)];
-    Matrix3d Rs[(WINDOW_SIZE + 1)];
-    Vector3d Bas[(WINDOW_SIZE + 1)];
-    Vector3d Bgs[(WINDOW_SIZE + 1)];
-    double td;
+  void slideWindow();
 
-    Matrix3d back_R0, last_R, last_R0;
-    Vector3d back_P0, last_P, last_P0;
-    std_msgs::Header Headers[(WINDOW_SIZE + 1)];
+  void solveOdometry();
 
-    IntegrationBase *pre_integrations[(WINDOW_SIZE + 1)];
-    Vector3d acc_0, gyr_0;
+  void slideWindowNew();
 
-    vector<double> dt_buf[(WINDOW_SIZE + 1)];
-    vector<Vector3d> linear_acceleration_buf[(WINDOW_SIZE + 1)];
-    vector<Vector3d> angular_velocity_buf[(WINDOW_SIZE + 1)];
+  void slideWindowOld();
 
-    int frame_count;
-    int sum_of_outlier, sum_of_back, sum_of_front, sum_of_invalid;
+  void optimization();
 
-    FeatureManager f_manager;
-    MotionEstimator m_estimator;
-    InitialEXRotation initial_ex_rotation;
+  void vector2double();
 
-    bool first_imu;
-    bool is_valid, is_key;
-    bool failure_occur;
+  void double2vector();
 
-    vector<Vector3d> point_cloud;
-    vector<Vector3d> margin_cloud;
-    vector<Vector3d> key_poses;
-    double initial_timestamp;
+  bool failureDetection();
 
 
-    double para_Pose[WINDOW_SIZE + 1][SIZE_POSE];
-    double para_SpeedBias[WINDOW_SIZE + 1][SIZE_SPEEDBIAS];
-    double para_Feature[NUM_OF_F][SIZE_FEATURE];
-    double para_Ex_Pose[NUM_OF_CAM][SIZE_POSE];
-    double para_Retrive_Pose[SIZE_POSE];
-    double para_Td[1][1];
-    double para_Tr[1][1];
+  enum SolverFlag {
+    INITIAL,
+    NON_LINEAR
+  };
 
-    int loop_window_index;
+  enum MarginalizationFlag {
+    MARGIN_OLD = 0,
+    MARGIN_SECOND_NEW = 1
+  };
 
-    MarginalizationInfo *last_marginalization_info;
-    vector<double *> last_marginalization_parameter_blocks;
+  SolverFlag solver_flag;
+  MarginalizationFlag marginalization_flag;
+  Vector3d g;
+  MatrixXd Ap[2], backup_A;
+  VectorXd bp[2], backup_b;
 
-    map<double, ImageFrame> all_image_frame;
-    IntegrationBase *tmp_pre_integration;
+  Matrix3d ric[NUM_OF_CAM];
+  Vector3d tic[NUM_OF_CAM];
 
-    //relocalization variable
-    bool relocalization_info;
-    double relo_frame_stamp;
-    double relo_frame_index;
-    int relo_frame_local_index;
-    vector<Vector3d> match_points;
-    double relo_Pose[SIZE_POSE];
-    Matrix3d drift_correct_r;
-    Vector3d drift_correct_t;
-    Vector3d prev_relo_t;
-    Matrix3d prev_relo_r;
-    Vector3d relo_relative_t;
-    Quaterniond relo_relative_q;
-    double relo_relative_yaw;
+  Vector3d Ps[(WINDOW_SIZE + 1)];
+  Vector3d Vs[(WINDOW_SIZE + 1)];
+  Matrix3d Rs[(WINDOW_SIZE + 1)];
+  Vector3d Bas[(WINDOW_SIZE + 1)];
+  Vector3d Bgs[(WINDOW_SIZE + 1)];
+  double td;
+
+  Matrix3d back_R0, last_R, last_R0;
+  Vector3d back_P0, last_P, last_P0;
+  std_msgs::Header Headers[(WINDOW_SIZE + 1)];
+
+  IntegrationBase* pre_integrations[(WINDOW_SIZE + 1)];
+  Vector3d acc_0, gyr_0;
+
+  vector<double> dt_buf[(WINDOW_SIZE + 1)];
+  vector<Vector3d> linear_acceleration_buf[(WINDOW_SIZE + 1)];
+  vector<Vector3d> angular_velocity_buf[(WINDOW_SIZE + 1)];
+
+  int frame_count;
+  int sum_of_outlier, sum_of_back, sum_of_front, sum_of_invalid;
+
+  FeatureManager f_manager;
+  MotionEstimator m_estimator;
+  InitialEXRotation initial_ex_rotation;
+
+  bool first_imu;
+  bool is_valid, is_key;
+  bool failure_occur;
+
+  vector<Vector3d> point_cloud;
+  vector<Vector3d> margin_cloud;
+  vector<Vector3d> key_poses;
+  double initial_timestamp;
+
+
+  double para_Pose[WINDOW_SIZE + 1][SIZE_POSE];
+  double para_SpeedBias[WINDOW_SIZE + 1][SIZE_SPEEDBIAS];
+  double para_Feature[NUM_OF_F][SIZE_FEATURE];
+  double para_Ex_Pose[NUM_OF_CAM][SIZE_POSE];
+  double para_Retrive_Pose[SIZE_POSE];
+  double para_Td[1][1];
+  double para_Tr[1][1];
+
+  int loop_window_index;
+
+  MarginalizationInfo* last_marginalization_info;
+  vector<double*> last_marginalization_parameter_blocks;
+
+  map<double, ImageFrame> all_image_frame;
+  IntegrationBase* tmp_pre_integration;
+
+  //relocalization variable
+  bool relocalization_info;
+  double relo_frame_stamp;
+  double relo_frame_index;
+  int relo_frame_local_index;
+  vector<Vector3d> match_points;
+  double relo_Pose[SIZE_POSE];
+  Matrix3d drift_correct_r;
+  Vector3d drift_correct_t;
+  Vector3d prev_relo_t;
+  Matrix3d prev_relo_r;
+  Vector3d relo_relative_t;
+  Quaterniond relo_relative_q;
+  double relo_relative_yaw;
 };
--- vins_estimator/src/estimator_node.cpp
+++ vins_estimator/src/estimator_node.cpp
@@ -31,24 +31,25 @@ double latest_time;
 Eigen::Vector3d tmp_P;
 Eigen::Quaterniond tmp_Q;
 Eigen::Vector3d tmp_V;
-Eigen::Vector3d tmp_Ba;
-Eigen::Vector3d tmp_Bg;
+Eigen::Vector3d tmp_bias_acc;
+Eigen::Vector3d tmp_bias_gyr;
 Eigen::Vector3d acc_0;
 Eigen::Vector3d gyr_0;
-bool init_feature = 0;
-bool init_imu = 1;
+bool features_initialized = false;
+bool imu_initialized = false;
 double last_imu_t = 0;
 
 void predict(const sensor_msgs::ImuConstPtr& imu_msg) {
   double t = imu_msg->header.stamp.toSec();
-  if (init_imu) {
+  if (!imu_initialized) {
     latest_time = t;
-    init_imu = 0;
+    imu_initialized = true;
     return;
   }
   double dt = t - latest_time;
   latest_time = t;
 
+  // raw measurements
   double dx = imu_msg->linear_acceleration.x;
   double dy = imu_msg->linear_acceleration.y;
   double dz = imu_msg->linear_acceleration.z;
@@ -59,18 +60,26 @@ void predict(const sensor_msgs::ImuConstPtr& imu_msg) {
   double rz = imu_msg->angular_velocity.z;
   Eigen::Vector3d angular_velocity{rx, ry, rz};
 
-  Eigen::Vector3d un_acc_0 = tmp_Q * (acc_0 - tmp_Ba) - estimator.g;
+  // rotate to world frame, subtract bias & gravity
+  Eigen::Vector3d un_acc_0 = tmp_Q * (acc_0 - tmp_bias_acc) - estimator.g;
 
-  Eigen::Vector3d un_gyr = 0.5 * (gyr_0 + angular_velocity) - tmp_Bg;
+  // average with prior on gyro data, subtract gyro bias
+  Eigen::Vector3d un_gyr = 0.5 * (gyr_0 + angular_velocity) - tmp_bias_gyr;
+
+  // propagate attitude
   tmp_Q = tmp_Q * Utility::deltaQ(un_gyr * dt);
 
-  Eigen::Vector3d un_acc_1 = tmp_Q * (linear_acceleration - tmp_Ba) - estimator.g;
+  // use propagated attitude to integrate actual accelerometer measurement
+  Eigen::Vector3d un_acc_1 = tmp_Q * (linear_acceleration - tmp_bias_acc) - estimator.g;
 
+  // average prior acceleration with posterior acceleration
   Eigen::Vector3d un_acc = 0.5 * (un_acc_0 + un_acc_1);
 
+  // update position & velocity
   tmp_P = tmp_P + dt * tmp_V + 0.5 * dt * dt * un_acc;
   tmp_V = tmp_V + dt * un_acc;
 
+  // update prior of accelerometer & gyro
   acc_0 = linear_acceleration;
   gyr_0 = angular_velocity;
 }
@@ -81,15 +90,15 @@ void update() {
   tmp_P = estimator.Ps[WINDOW_SIZE];
   tmp_Q = estimator.Rs[WINDOW_SIZE];
   tmp_V = estimator.Vs[WINDOW_SIZE];
-  tmp_Ba = estimator.Bas[WINDOW_SIZE];
-  tmp_Bg = estimator.Bgs[WINDOW_SIZE];
+  tmp_bias_acc = estimator.Bas[WINDOW_SIZE];
+  tmp_bias_gyr = estimator.Bgs[WINDOW_SIZE];
   acc_0 = estimator.acc_0;
   gyr_0 = estimator.gyr_0;
 
   queue<sensor_msgs::ImuConstPtr> tmp_imu_buf = imu_buf;
-  for (sensor_msgs::ImuConstPtr tmp_imu_msg; !tmp_imu_buf.empty(); tmp_imu_buf.pop())
+  for (sensor_msgs::ImuConstPtr tmp_imu_msg; !tmp_imu_buf.empty(); tmp_imu_buf.pop()) {
     predict(tmp_imu_buf.front());
-
+  }
 }
 
 std::vector<std::pair<std::vector<sensor_msgs::ImuConstPtr>, sensor_msgs::PointCloudConstPtr>>
@@ -146,15 +155,17 @@ void imu_callback(const sensor_msgs::ImuConstPtr& imu_msg) {
     predict(imu_msg);
     std_msgs::Header header = imu_msg->header;
     header.frame_id = "world";
-    if (estimator.solver_flag == Estimator::SolverFlag::NON_LINEAR)
+    if (estimator.solver_flag == Estimator::SolverFlag::NON_LINEAR) {
+      ROS_INFO("pubLatestOdometry");
       pubLatestOdometry(tmp_P, tmp_Q, tmp_V, header);
+    }
   }
 }
 
 void feature_callback(const sensor_msgs::PointCloudConstPtr& feature_msg) {
-  if (!init_feature) {
+  if (!features_initialized) {
     //skip the first detected feature, which doesn't contain optical flow speed
-    init_feature = 1;
+    features_initialized = true;
     return;
   }
   mutex_buf.lock();
